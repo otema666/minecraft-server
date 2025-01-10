5ad76565d2ea9a4cee4dc336668eff0c76aedd28
otema <otema000@gmail.com>
2025-01-10 14:32:23 +0100

    upd

--- start.sh
+++ start.sh
@@ -1,37 +1,5 @@
 #!/bin/bash
+screen -S minecraft -d -m java -Xmx2048M -jar paper-1.21.1-128.jar nogui
 
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m'
-
-if screen -list | grep -q "minecraft"; then
-  echo -e "${RED}[-] El servidor de Minecraft ya está en ejecución!${NC}"
-  exit 1
-fi
-
-echo -e "${GREEN}[+] Iniciando el servidor de Minecraft...${NC}\n"
-clear
-screen -S minecraft -d -m java -Xmx1024M -jar paper-1.21.1-128.jar nogui
-echo -e "${GREEN}[+] Servidor en ejecución. Para ver la consola, use el comando 'screen -r minecraft'.${NC}\n"
-cleanup() {
-  echo -e "${GREEN}[+] Deteniendo el servidor de Minecraft...${NC}"
-  screen -S minecraft -p 0 -X stuff "stop$(printf '\r')"
-  sleep 5
-  echo -e "${GREEN}[+] Server stopped. Exiting script.${NC}"
-  exit 0
-}
-
-trap cleanup SIGINT
-
-auto_save() {
-echo -e "${GREEN}[+] Iniciando el guardado automático del mundo...${NC}"
-  while screen -list | grep -q "minecraft"; do
-    sleep 500
-    echo -e "${GREEN}[+] Guardando mundo...${NC}"
-    screen -S minecraft -p 0 -X stuff "say Mundo Guardado!$(printf '\r')"
-    sleep 5
-    screen -S minecraft -p 0 -X stuff "save-all$(printf '\r')"
-  done
-}
-
-auto_save
+echo "[+] Server started"
+echo "[-] Para ver la consola del servidor, ejecuta el comando 'screen -r minecraft'"
--- uninstall.sh
+++ uninstall.sh
@@ -7,10 +7,6 @@ NC='\033[0m'
 echo -e "${GREEN}Stopping Minecraft server...${NC}"
 pkill -f paper-1.21.1-128.jar || echo -e "${GREEN}No running Minecraft server found.${NC}"
 
-# Eliminar el directorio de Minecraft
-echo -e "${GREEN}Removing Minecraft directory...${NC}"
-rm -rf /home/otema/minecraft
-
 # Desinstalar Java
 echo -e "${GREEN}Uninstalling default JRE...${NC}"
 sudo apt remove --purge -y default-jre
@@ -20,3 +16,6 @@ echo -e "${GREEN}Removing unused packages...${NC}"
 sudo apt autoremove -y
 
 echo -e "${GREEN}Minecraft and Java have been uninstalled.${NC}"
+
+# Eliminar el directorio de Minecraft
+echo -e "${GREEN}Por favor, elimine el directorio de Minecraft manualmente.${NC}"
